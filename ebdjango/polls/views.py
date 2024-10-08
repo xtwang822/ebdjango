d41ceac7ee65aaa4a40c3d62f08dcc59f3adf9f5
xtwang822 <xtwang2024@hotmail.com>
2024-10-07 21:10:48 -0400

    fix polls/results err

--- ebdjango/polls/views.py
+++ ebdjango/polls/views.py
@@ -5,41 +5,7 @@ from django.views import generic
 
 from .models import Choice, Question
 
-# def index(request):
-#     # latest_question_list = Question.objects.order_by('-pub_date')[:5]
-#     # template = loader.get_template('polls/index.html')
-#     # context = {
-#     #     'latest_question_list': latest_question_list,
-#     # }
-#     # return HttpResponse(template.render(context, request))
-#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-#     context = {'latest_question_list': latest_question_list}
-#     return render(request, 'polls/index.html', context)
-#
-# # def index(request):
-# #     return HttpResponse("Hello, world. You're at the polls index.")
-#
-# # def detail(request, question_id):
-# #     return HttpResponse("You're looking at question %s." % question_id)
-#
-# # def detail(request, question_id):
-# #     try:
-# #         question = Question.objects.get(pk=question_id)
-# #     except Question.DoesNotExist:
-# #         raise Http404("Question does not exist")
-# #     return render(request, 'polls/detail.html', {'question': question})
-# #
-# def detail(request, question_id):
-#     question = get_object_or_404(Question, pk=question_id)
-#     return render(request, 'polls/detail.html', {'question': question})
-#
-# # def results(request, question_id):
-# #     response = "You're looking at the results of question %s."
-# #     return HttpResponse(response % question_id)
-#
-# # def vote(request, question_id):
-# #     return HttpResponse("You're voting on question %s." % question_id)
-#
+
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
@@ -57,10 +23,6 @@ def vote(request, question_id):
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
-#
-# def results(request, question_id):
-#     question = get_object_or_404(Question, pk=question_id)
-#     return render(request, 'polls/results.html', {'question': question})
 
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
@@ -70,11 +32,9 @@ class IndexView(generic.ListView):
         """Return the last five published questions."""
         return Question.objects.order_by('-pub_date')[:5]
 
-
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
-
 
 class ResultsView(generic.DetailView):
     model = Question
